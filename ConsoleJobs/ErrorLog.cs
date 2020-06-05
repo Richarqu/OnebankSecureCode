17a0a453ed97b5c146dc68305de0478672e320c0
Richarqu <richarque@gmail.com>
2020-06-05 12:12:04 +0100

    added error log file and tested fiorano connection

---
+++ ConsoleJobs/ErrorLog.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleJobs
+{
+    class ErrorLog
+    {
+
+        public ErrorLog(Exception ex)
+        {
+            string pth = ConfigurationManager.AppSettings["ErrorLogPath"];
+            if (!Directory.Exists(pth))
+            {
+                Directory.CreateDirectory(pth);
+            }
+            string err = ex.ToString();
+            //string err = ex.Message;
+
+            DateTime dt = DateTime.Now;
+            string fld = dt.ToString("yyyy") + "_" + dt.ToString("MM") + "_";
+            pth += fld + dt.ToString("dd") + ".txt";
+            if (!File.Exists(pth))
+            {
+                using (StreamWriter sw = File.CreateText(pth))
+                {
+                    sw.WriteLine(dt.ToString() + " : " + err);
+                    sw.WriteLine(" ");
+                    sw.Close();
+                    sw.Dispose();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(pth))
+                {
+                    sw.WriteLine(dt.ToString() + " : " + err);
+                    sw.WriteLine(" ");
+                    sw.Close();
+                    sw.Dispose();
+                }
+
+            }
+            Console.WriteLine(ex.ToString());
+        }
+        public ErrorLog(string ex)
+        {
+            string pth = ConfigurationManager.AppSettings["ErrorLogPath"];
+
+            if (!Directory.Exists(pth))
+            {
+                Directory.CreateDirectory(pth);
+            }
+            string err = ex;
+            DateTime dt = DateTime.Now;
+            string fld = dt.ToString("yyyy") + "_" + dt.ToString("MM") + "_";
+            pth += fld + dt.ToString("dd") + ".txt";
+            if (!File.Exists(pth))
+            {
+                using (StreamWriter sw = File.CreateText(pth))
+                {
+                    sw.WriteLine(dt.ToString() + " : " + err);
+                    sw.WriteLine(" ");
+                    sw.Close();
+                    sw.Dispose();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(pth))
+                {
+                    sw.WriteLine(dt.ToString() + " : " + err);
+                    sw.WriteLine(" ");
+                    sw.Close();
+                    sw.Dispose();
+                }
+
+            }
+            Console.WriteLine(ex);
+        }
+    }
+}
